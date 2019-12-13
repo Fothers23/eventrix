53c1c25ce70fcaf49987b02683bd35067f0d4434
Giorgos Andreou <gandreou@alacrityfoundation.com>
2019-12-13 15:43:59 +0000

    made models and controllers

---
+++ app/Http/Controllers/CapRoomStyleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CapRoomStyle;
+use Illuminate\Http\Request;
+
+class CapRoomStyleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CapRoomStyle  $capRoomStyle
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CapRoomStyle $capRoomStyle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CapRoomStyle  $capRoomStyle
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CapRoomStyle $capRoomStyle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CapRoomStyle  $capRoomStyle
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CapRoomStyle $capRoomStyle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CapRoomStyle  $capRoomStyle
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CapRoomStyle $capRoomStyle)
+    {
+        //
+    }
+}
