cf0a9def5829fe19cafc3fad0ab63b84d453bb44
Sotiris Hadjipanayi <sothadjipanayi@gmail.com>
2019-12-13 16:07:01 +0000

    5 one-way relationships done

--- app/SicDivision.php
+++ app/SicDivision.php
@@ -7,4 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class SicDivision extends Model
 {
     //
+    protected $fillable = [
+        'name', 'code',
+    ];
+
+    public function sicSection() 
+    {
+        return $this->belongsTo(SicSection::class);
+    }
+
+    /*
+        Relationship: Many to Many
+        Return: Collection
+    */
+    public function EventInstance() 
+    {
+        return $this->belongsToMany(EventInstance::class);
+    }
+
 }
--- app/SicSection.php
+++ app/SicSection.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class SicSection extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'code', 'image'
+    ];
+
+    /*
+        Relationship: One to Many
+        Return: Collection
+    */
+    public function sicDivisions() 
+    {
+        return $this->hasMay(SicDivision::class);
+    }
 }
