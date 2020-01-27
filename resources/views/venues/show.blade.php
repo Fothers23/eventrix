9023db15bdbc27c743c02e4a152f0788d5032769
andreou95 <andreou.uni@hotmail.com>
2020-01-27 10:27:13 +0000

    Update show.blade.php

--- resources/views/venues/show.blade.php
+++ resources/views/venues/show.blade.php
@@ -19,7 +19,7 @@
             <p><b>City: </b>{{$venue->city}}</p>
             <p><b>Post Code: </b>{{$venue->post_code}}</p>
             <p><b>Email: </b>{{$venue->email}}</p>
-            <p><b>Phone number: </b>{{$venue->post_code}}</p>
+            <p><b>Phone number: </b>{{$venue->phone_number}}</p>
             <p><b>Research Notes: </b>{{$venue->research_notes}}</p>
             <div class="row">
                 <a href="{{route('venues.index')}}" class="btn btn-primary">Back</a>
